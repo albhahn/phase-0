09e4a5940390be2f63c6fa488af99c0a5b27a1eb
Albert Hahn <xaphx@yahoo.com>
2015-11-14 16:24:10 -0800

    Complete 4.7.1 Excercise

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,22 @@
+# Factorial
+
+# I worked on this challenge with: Nick Davies.
+
+
+# Your Solution Below
+
+# Create a method that takes in a positive integer and calculates the factorial of that number.
+# WHILE number is greater than 0, method will count down and multiply the return value by the counter.
+# IF number is equal to 0, return 1.
+
+def factorial(number)
+  if number == 0
+    return 1
+  end
+    factorial = 1
+  while number > 1
+    factorial = factorial * number
+    number = number - 1
+  end
+  return factorial
+end
