d0eaf22cbe1da5b5999fb71fe956e12750cf6396
Albert Hahn <xaphx@yahoo.com>
2015-12-07 03:28:05 -0800

    Complete 7.8 group challenge

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,35 @@
+/*
+// Release 2 - Pseudocode:
+
+// Create function that will return the sum of all numbers of a given array
+Input: An array of numbers
+Output: Sum of all numbers in the array
+Steps:
+1. Define a function for "sum", which takes in an array of integers
+2. Declare a variable "total_sum", which is equal to 0
+3. Iterate through each element in the array
+4. Add each element in the array to "total_sum"
+5. Return "total_sum"
+
+// Create function that will return the mean of all numbers of a given array
+Input: An array of numbers
+Output: The mean (average) of all numbers in the array
+Steps:
+1. Define a function for "mean", which takes in an array of integers
+2. Declare a variable "average", which is equal to 0
+3. Iterate through each element in the array
+4. Add each element in the array to "average"
+5. Divide "average" by the length of the array
+6. Return "average"
+
+// Create function that will return the median of all numbers a given array
+Input: An array of numbers
+Output: The median of all numbers in the array
+Steps:
+1. Define a function for "median", which takes in an array of intergers
+2. Sort the array in order from least to greatest
+3. Declare a variable equal to the length of the list and divide it by 2.
+4. IF the length of the array is odd, return the variable(length / 2)
+5. ELSE, return the variable(length/2) + variable[(length/2) - 1] / 2
+
+*/
